5b6093695e533176a1f4839c2b9f95dfca50949b
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-13 06:39:13 -0400

    Initial commit

---
+++ source/false_paradise/FalseParadiseShoutoutsState.hx
@@ -0,0 +1,21 @@
+package false_paradise;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.transition.FlxTransitionableState;
+
+class FalseParadiseShoutoutsState extends FlxTransitionableState
+{
+	override function create()
+	{
+		var credits = new FlxSprite().loadGraphic('mod:mod_assets/FalseParadiseMod/preload/weeks/credits.png');
+		credits.antialiasing = true;
+		add(credits);
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
